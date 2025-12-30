061cc5930c0fdb6ae5874caa676bd660448c26c9
Leon Jelsma <leonjelsma@gmail.com>
2025-12-30 12:02:44 +0100

    Fix double opening path

--- main.py
+++ main.py
@@ -8,21 +8,18 @@ from const import IMAGES_DIR
 from lib import epd13in3E
 from utils.image_utils import enhance_colors, resize_for_spectra6, count_images, image_generator
 
-if not os.path.exists(IMAGES_DIR):
-    os.makedirs(IMAGES_DIR)
-
 screen = epd13in3E.EPD()
 
 logger = logging.getLogger(__name__)
 
 try:
+    logger.info("PiFrame started, initializing display")
+
     screen.Init()
-    logger.info("PiFrame started!")
     screen.Clear()
 
-    for image_path in image_generator(IMAGES_DIR):
+    for image in image_generator(IMAGES_DIR):
         print("Drawing test.JPEG")
-        image = Image.open(os.path.join(image_path))
         image = enhance_colors(image)
         image = resize_for_spectra6(image)
         screen.display(screen.getbuffer(image))
